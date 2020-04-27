609ac1f2352c95afc5cd1bc332cb3d6f63873639
Alex Botelho <alexbotelho1@hotmail.com>
2020-04-26 21:20:03 -0300

    Masterclass 1 - Desafio finalizado

--- exercicios/public/scripts.js
+++ exercicios/public/scripts.js
@@ -2,6 +2,20 @@ const ul = document.querySelector("ul")
 const input = document.querySelector("input")
 const form = document.querySelector('form')
 
+async function loadUrlsJson() {
+   const res = await fetch('http://localhost:3000').then((data) => data.json())
+   res.urls.map(({name, url}) => addElement({name, url}))
+}
+
+async function createUrlJson(name, url) {
+   await fetch(`http://localhost:3000?name=${name}&url=${url}`)
+}
+
+async function removeUrlJson(name, url) {
+   await fetch(`http://localhost:3000?name=${name}&url=${url}&del=1`)
+}
+
+loadUrlsJson()
 
 function addElement({ name, url }) {
     const li = document.createElement('li')
@@ -13,35 +27,37 @@ function addElement({ name, url }) {
     a.target = "_blank"
 
     trash.innerHTML = "x"
-    trash.onclick = () => removeElement(trash)
+    trash.onclick = () => removeElement(trash, name, url)
 
     li.append(a)
     li.append(trash)
     ul.append(li)
 }
 
-function removeElement(el) {
-    if (confirm('Tem certeza que deseja deletar?'))
-        el.parentNode.remove()
+function removeElement(el, name, url) {
+   if (confirm('Tem certeza que deseja deletar?'))
+      el.parentNode.remove()
+   removeUrlJson(name, url)
 }
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+   event.preventDefault();
 
-    let { value } = input
+   let { value } = input
 
-    if (!value) 
-        return alert('Preencha o campo')
+   if (!value) 
+      return alert('Preencha o campo')
 
-    const [name, url] = value.split(",")
+   const [name, url] = value.split(",")
 
-    if (!url) 
-        return alert('formate o texto da maneira correta')
+   if (!url) 
+      return alert('formate o texto da maneira correta')
 
-    if (!/^http/.test(url)) 
-        return alert("Digite a url da maneira correta")
+   if (!/^http/.test(url)) 
+      return alert("Digite a url da maneira correta")
 
-    addElement({ name, url })
+   createUrlJson(name, url)
+   addElement({ name, url })
 
-    input.value = ""
+   input.value = ""
 })
